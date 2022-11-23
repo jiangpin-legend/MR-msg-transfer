fd2eda01adfecc60c5cffd10d692158cd9f7246f
jiangpin-legend <3180105573@zju.edu.cn>
2022-11-23 16:30:53 +0800

    modified sync.sh

--- sync.sh
+++ sync.sh
@@ -1,4 +1,4 @@
 
-rsync -r scripts/ nx_1:/home/nx/MR/multi-robot-msg-transfer/src/msg-transfer/scripts
-rsync -r scripts/ nx_2:/home/nx/MR/multi-robot-msg-transfer/src/msg-transfer/scripts
-rsync -r scripts/ lusha:/home/lusha/github/MR/multi-robot-msg-transfer/src/msg-transfer/scripts
+rsync -r ../msg-transfer/ nx_1:/home/nx/MR/multi-robot-msg-transfer/src/msg-transfer/
+rsync -r ../msg-transfer/ nx_2:/home/nx/MR/multi-robot-msg-transfer/src/msg-transfer/
+rsync -r ../msg-transfer/ lusha:/home/lusha/github/MR/multi-robot-msg-transfer/src/msg-transfer/
